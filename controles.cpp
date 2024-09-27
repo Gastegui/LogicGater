1bed0ee3aa771035070a97ad89e13aaa7e28a565
Gastegui <gastedev@gmail.com>
2024-09-27 13:24:58 +0200

    Mejorado sistema de controles para permitir modificar los controles fácilmente (sin implementar el poder modificar los controles desde el juego)

--- controles.cpp
+++ controles.cpp
@@ -4,107 +4,81 @@
 
 #include "controles.h"
 
+#include <iostream>
+
 Controles::Accion Controles::accionAnterior = Nada;
 SDL_Event* Controles::evento = nullptr;
+std::map<std::string, Controles::Accion> Controles::acciones;
 
 Controles::Accion Controles::getNuevaAccion(const SDL_Event* evento)
 {
     accionAnterior = Nada;
+    std::string str = "--";
+    if(evento->type == SDL_KEYDOWN || evento->type == SDL_MOUSEBUTTONDOWN)
+        str[0] = 'p';
+    else if (evento->type == SDL_KEYUP || evento->type == SDL_MOUSEBUTTONUP)
+        str[0] = 's';
     switch (evento->type)
     {
+        case SDL_MOUSEBUTTONDOWN:
+        case SDL_MOUSEBUTTONUP:
+            if(evento->button.button == SDL_BUTTON_LEFT)
+                str[1] = 'I';
+            else if(evento->button.button == SDL_BUTTON_MIDDLE)
+                str[1] = 'M';
+            else if(evento->button.button == SDL_BUTTON_RIGHT)
+                str[1] = 'D';
+            break;
+        case SDL_KEYDOWN:
+        case SDL_KEYUP:
+            if((evento->key.keysym.sym >= 'a' && evento->key.keysym.sym <= 'z') || (evento->key.keysym.sym >= '0' && evento->key.keysym.sym <= '9'))
+                str[1] = static_cast<char>(evento->key.keysym.sym);
+            else
+            {
+                switch(evento->key.keysym.sym)
+                {
+                    case SDLK_SPACE:
+                        str[1] = 'E';
+                        break;
+                    case SDLK_RETURN:
+                        str[1] = 'R';
+                        break;
+                    case SDLK_BACKSPACE:
+                        str[1] = 'B';
+                        break;
+                    case SDLK_TAB:
+                        str[1] = 'T';
+                        break;
+                    case SDLK_LCTRL:
+                    case SDLK_RCTRL:
+                        str[1] = 'C';
+                        break;
+                    case SDLK_LSHIFT:
+                    case SDLK_RSHIFT:
+                        str[1] = 'S';
+                        break;
+                    case SDLK_LALT:
+                    case SDLK_RALT:
+                        str[1] = 'A';
+                        break;
+                    case SDLK_ESCAPE:
+                        str[1] = 'X';
+                        break;
+                    default:
+                        break;
+                }
+            }
+            break;
         case SDL_QUIT:
             accionAnterior = Cerrar;
             break;
         case SDL_MOUSEMOTION:
             accionAnterior = MovimientoRaton;
             break;
-        case SDL_MOUSEBUTTONDOWN:
-            switch (evento->button.button)
-            {
-                case SDL_BUTTON_LEFT:
-                    accionAnterior = InteractuarAbajo;
-                    break;
-                case SDL_BUTTON_MIDDLE:
-                    accionAnterior = ConexionAbajo;
-                    break;
-                case SDL_BUTTON_RIGHT:
-                    accionAnterior = MoverAbajo;
-                    break;
-                default:
-                    break;
-            }
-            break;
-        case SDL_MOUSEBUTTONUP:
-            switch (evento->button.button)
-            {
-                case SDL_BUTTON_LEFT:
-                    accionAnterior = Interactuar;
-                    break;
-                case SDL_BUTTON_MIDDLE:
-                    accionAnterior = ConexionArriba;
-                    break;
-                case SDL_BUTTON_RIGHT:
-                    accionAnterior = MoverArriba;
-                    break;
-                default:
-                    break;
-            }
-            break;
-        case SDL_KEYDOWN:
-            switch (evento->key.keysym.sym)
-            {
-                case SDLK_SPACE:
-                    accionAnterior = ConexionAbajo;
-                    break;
-                default:
-                    break;
-            }
-            break;
-        case SDL_KEYUP:
-            switch(evento->key.keysym.sym)
-            {
-                case SDLK_SPACE:
-                    accionAnterior = ConexionArriba;
-                    break;
-                case SDLK_BACKSPACE:
-                    accionAnterior = AlternarBorrando;
-                    break;
-                case SDLK_RETURN:
-                    accionAnterior = SimularPaso;
-                    break;
-                case 'q':
-                    accionAnterior = SimularEmpezar;
-                    break;
-                case 'w':
-                    accionAnterior = SimularParar;
-                    break;
-                case 'l':
-                    accionAnterior = BorrarSueltos;
-                    break;
-                case 'a':
-                    accionAnterior = CrearAnd;
-                    break;
-                case 'o':
-                    accionAnterior = CrearOr;
-                    break;
-                case 'x':
-                    accionAnterior = CrearXor;
-                    break;
-                case 'i':
-                    accionAnterior = CrearInterruptor;
-                    break;
-                case 'b':
-                    accionAnterior = CrearBoton;
-                    break;
-                case 's':
-                    accionAnterior = CrearSalida;
-                    break;
-                default:
-                    break;
-            }
-            break;
         default:
             break;
     }
+    if(str != "--" && acciones.contains(str))
+        accionAnterior = acciones[str];
     return accionAnterior;
 }
--- controles.h
+++ controles.h
@@ -4,7 +4,9 @@
 
 #ifndef CONTROLES_H
 #define CONTROLES_H
+#include <map>
 #include <SDL_events.h>
+#include <string>
 
 
 class Controles
@@ -38,11 +40,31 @@ public:
 private:
     static Accion accionAnterior;
     static SDL_Event* evento;
+    static std::map<std::string, Accion> acciones;
+    //Primer caracter p: pulsar s: soltar
+    //Segundo caracter I: raton izquierda. M: raton medio. D: raton derecha. E: espacio. R: enter. B: borrar. T: tab. C: ctrl. S: shift. A: alt. X: escape. Cualquier caracter en minusculas: su tecla en el teclado
 public:
     static void init(SDL_Event* event)
     {
         evento = event;
         accionAnterior = Nada;
+        acciones.insert(std::make_pair("pI", InteractuarAbajo));
+        acciones.insert(std::make_pair("sI", Interactuar));
+        acciones.insert(std::make_pair("pD", MoverAbajo));
+        acciones.insert(std::make_pair("sD", MoverArriba));
+        acciones.insert(std::make_pair("pE", ConexionAbajo));
+        acciones.insert(std::make_pair("sE", ConexionArriba));
+        acciones.insert(std::make_pair("sB", AlternarBorrando));
+        acciones.insert(std::make_pair("sl", BorrarSueltos));
+        acciones.insert(std::make_pair("sa", CrearAnd));
+        acciones.insert(std::make_pair("so", CrearOr));
+        acciones.insert(std::make_pair("sx", CrearXor));
+        acciones.insert(std::make_pair("si", CrearInterruptor));
+        acciones.insert(std::make_pair("sb", CrearBoton));
+        acciones.insert(std::make_pair("ss", CrearSalida));
+        acciones.insert(std::make_pair("sR", SimularPaso));
+        acciones.insert(std::make_pair("sq", SimularEmpezar));
+        acciones.insert(std::make_pair("sw", SimularParar));
     }
     static Accion getNuevaAccion(const SDL_Event* evento);
     [[nodiscard]] static Accion getUltimaAccion() { return accionAnterior; }
